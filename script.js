1e30960bde1fcb7f489a24315871155e3f0cbe96
Romcha Levedev <lr.mos@mail.ru>
2022-08-28 18:08:36 +0300

    added task 2

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function chekEvenNumber() {
     const resultEnput = enputEl.querySelector('[readonly]')
 
     const buttonSubmit = enputEl.querySelector('button');
-    console.log(buttonSubmit);
 
     buttonSubmit.addEventListener('click', (e) => {
         const data = queryInputEl.value
@@ -21,3 +20,35 @@ function chekEvenNumber() {
 }
 
 chekEvenNumber()
+
+function calculate() {
+    const beginEl = document.querySelector('.begin__form')
+    const firstOperand = beginEl.querySelector('input')
+    const secondOperand = beginEl.querySelector('[name="operand-2"]')
+    const operator = beginEl.querySelector('select')
+    const result = beginEl.querySelector('[readonly]')
+    const buttonSubmit = beginEl.querySelector('button')
+
+    buttonSubmit.addEventListener('click', (e) => {
+
+        const a = firstOperand.value
+        const b = secondOperand.value
+        const sign = operator.value
+
+        if (isNaN(a) || isNaN(b)) {
+            result.value = 'NaN'
+        } else {
+            sign === 'plus' && (result.value = parseInt(a) + parseInt(b))
+            sign === 'subtraction' && (result.value = a - b)
+            sign === 'divide' && (result.value = (a / b).toFixed(2))
+            sign === 'multiplication' && (result.value = a * b)
+        }
+
+        e.preventDefault()
+        // ? почему не работает я же задал + ?
+        // ? if (sign === '+') {
+        // ?    result.value = parseInt(a) + parseInt(b)
+    });
+}
+
+calculate()
