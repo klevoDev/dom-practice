d9dbdc2abf0ddcfb2b8c45fb87d329bdcdee4f31
Romcha Levedev <lr.mos@mail.ru>
2022-08-29 12:43:21 +0300

    added task 4

--- script.js
+++ script.js
@@ -57,20 +57,24 @@ function getDefaultСolor() {
 
     const sectionBackEl = document.querySelector('.page__back');
     const selectedColor = sectionBackEl.querySelector('[selected]')
+    const buttonEl = sectionBackEl.querySelector('button')
+    const buttonDefault = buttonEl.nextElementSibling
     const bodyEl = sectionBackEl.closest('body')
     const colorBackEl = selectedColor.value
 
-    if (colorBackEl === 'gray') {
-        bodyEl.style.backgroundColor = "#ccc"
-    } else if (colorBackEl === 'dark-gray') {
-        bodyEl.style.backgroundColor = "#332f2c"
-    } else if (colorBackEl === 'light-yellow') {
-        bodyEl.style.backgroundColor = "#ffffcc"
-    } else if (colorBackEl === 'orange') {
-        bodyEl.style.backgroundColor = "#ff9640"
-    } else if (colorBackEl === 'white') {
-        bodyEl.style.backgroundColor = "#fff"
-    }
+    buttonDefault.addEventListener('click', (event) => {
+        if (colorBackEl === 'gray') {
+            bodyEl.style.backgroundColor = "#ccc"
+        } else if (colorBackEl === 'dark-gray') {
+            bodyEl.style.backgroundColor = "#332f2c"
+        } else if (colorBackEl === 'light-yellow') {
+            bodyEl.style.backgroundColor = "#ffffcc"
+        } else if (colorBackEl === 'orange') {
+            bodyEl.style.backgroundColor = "#ff9640"
+        } else if (colorBackEl === 'white') {
+            bodyEl.style.backgroundColor = "#fff"
+        }
+    })
 }
 getDefaultСolor()
 
@@ -99,5 +103,61 @@ function changeBackground() {
 changeBackground()
 
 
+function lottery() {
 
+    const lotoEl = document.querySelector('.loto__form')
+    console.log(lotoEl);
+    const buttonEl = lotoEl.querySelector('button')
+    console.log(buttonEl);
 
+    const checkBox1 = lotoEl.querySelector('[name="lot-1"]')
+    const checkBox2 = lotoEl.querySelector('[name="lot-2"]')
+    const checkBox3 = lotoEl.querySelector('[name="lot-3"]')
+    const checkBox4 = lotoEl.querySelector('[name="lot-4"]')
+    const checkBox5 = lotoEl.querySelector('[name="lot-5"]')
+
+    let countTap = '';
+
+    buttonEl.addEventListener('click', (e) => {
+
+        let onCheckBox = parseInt(Math.random() * (5 - 1 + 1) + 1);
+
+        countTap += String(onCheckBox)
+
+        if (countTap.length > 3) {
+            buttonEl.disabled = true
+        }
+
+        if (onCheckBox === 1) {
+            (checkBox1.disabled = false)
+        } else if (onCheckBox === 2) {
+            checkBox1.disabled = false
+            checkBox2.disabled = false
+        } else if (onCheckBox === 3) {
+            checkBox1.disabled = false
+            checkBox2.disabled = false
+            checkBox3.disabled = false
+        } else if (onCheckBox === 4) {
+            checkBox1.disabled = false
+            checkBox2.disabled = false
+            checkBox3.disabled = false
+            checkBox4.disabled = false
+        } else if (onCheckBox === 5) {
+            checkBox1.disabled = false
+            checkBox2.disabled = false
+            checkBox3.disabled = false
+            checkBox4.disabled = false
+            checkBox5.disabled = false
+
+            checkBox1.checked = true
+            checkBox2.checked = true
+            checkBox3.checked = true
+            checkBox4.checked = true
+            checkBox5.checked = true
+
+            alert("Вы выиграли приз.")
+        }
+        e.preventDefault()
+    })
+}
+lottery()
